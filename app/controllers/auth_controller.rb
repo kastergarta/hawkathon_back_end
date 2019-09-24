bc7ad2fdb3381fd5814d781f3ce820f73bd0d170
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-09-24 19:02:56 -0400

    minor auth errors fixed

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,6 +2,7 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
+    
     @user = User.find_by(name: user_login_params[:name])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
